8991f0448ef8dee3d085256d1ed86db02c11caae
Kaito <rars213@gmail.com>
2020-04-26 12:40:34 -0500

    Fixed file, this is a working copy of "Default/Login" screen. Has button and placeholder txt

--- earthxhack1/App.js
+++ earthxhack1/App.js
@@ -1,39 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Actions, Router, Scene } from 'react-native-router-flux';
+//import { Actions, Router, Scene } from 'react-native-router-flux';
 import logo from './assets/Full_Fill_Logo.png';
-import LogIn from './LogIn.js';
+//import LogIn from './LogIn.js';
 
 export default function App() {
   return (
-    <Router>
-          <Scene
-            key="root">
-                <Scene>
-                    key= "LogIn"
-                    component={LogIn}
-                    title="LogIn"
-                </Scene>
-          
-          </Scene>
-    
+
     <View style={styles.container}>
           
           <Image source={logo} style={styles.logo}/>
           <Text style={styles.instructions}>
-          Username</Text>
+            Username</Text>
           <Text style={styles.instructions} >
             Password
           </Text>
         
           <TouchableOpacity
-          onPress={() => Actions.LogIn()}
-          style={styles.button}>
-          <Text style={styles.buttonText}>Click to Continue</Text>
+            onPress={() => alert('this is a button!')} style={styles.button}>
+            <Text style={styles.buttonText}> Click to Continue </Text>
           </TouchableOpacity>
           
     </View>
-          </Router>
+    //    </Router>
   );
 }
 

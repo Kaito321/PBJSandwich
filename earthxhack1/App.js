2cbca40679d8101e25005dfd9dcaf78a43291f4e
Kaito <rars213@gmail.com>
2020-04-26 14:47:29 -0500

    Navigation Working, begin connecting scenes

--- earthxhack1/App.js
+++ earthxhack1/App.js
@@ -3,37 +3,38 @@
 
 //import React from 'react';
 //import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-//import logo from './assets/Full_Fill_Logo.png';
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Image, StyleSheet, Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import logo from './assets/Full_Fill_Logo.png';
+import LogIn from './LogIn.js'
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>HOME</Text>
+          <Image source={logo} style={styles.logo}/>
+          <Text style={styles.instructions}>
+              Username</Text>
+          <Text style={styles.instructions} >
+              Password
+          </Text>
       <Button
           title="Go To LogIn"
-          onPress={() => navigation.navigate('Details')}
-    />
+          onPress={() => navigation.navigate(LogIn)} style={styles.button}>
+                     <Text style={styles.buttonText}> LogIn </Text>
+        </Button>
     </View>
   );
 }
-function LogIn() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="FULL->FILL" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="LogIn" component={LogIn} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -64,7 +65,7 @@ export default function App() {
         </View>
   );
 }
-
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -74,7 +75,7 @@ const styles = StyleSheet.create({
   backgroundColor: '#FFFDD4'
   },
 logo: {
-
+    //alignItems: 'Top',
     width: 305,
     height: 159,
     marginBottom: 10,
@@ -95,4 +96,4 @@ buttonText: {
     color: '#fff',
 }
 });
-*/
+

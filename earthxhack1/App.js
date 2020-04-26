d4ad9b19c4aa520f2728097671bd78390a4d0e2c
Kaito <rars213@gmail.com>
2020-04-26 14:03:31 -0500

    Testing navigation componenents!

--- earthxhack1/App.js
+++ earthxhack1/App.js
@@ -1,28 +1,67 @@
-import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-//import { Actions, Router, Scene } from 'react-native-router-flux';
-import logo from './assets/Full_Fill_Logo.png';
-//import LogIn from './LogIn.js';
+//****************Im APP.JS************************
 
+
+//import React from 'react';
+//import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+//import logo from './assets/Full_Fill_Logo.png';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>HOME</Text>
+      <Button
+          title="Go To LogIn"
+          onPress={() => navigation.navigate('Details')}
+    />
+    </View>
+  );
+}
+function LogIn() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="FULL->FILL" component={HomeScreen} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+//ORGINAL APP DESIGN
+/*
 export default function App() {
   return (
-
-    <View style={styles.container}>
-          
-          <Image source={logo} style={styles.logo}/>
-          <Text style={styles.instructions}>
+        <View style={styles.container}>
+      
+        <Image source={logo} style={styles.logo}/>
+        <Text style={styles.instructions}>
             Username</Text>
-          <Text style={styles.instructions} >
+        <Text style={styles.instructions} >
             Password
-          </Text>
-        
-          <TouchableOpacity
+        </Text>
+    
+        <TouchableOpacity
             onPress={() => alert('this is a button!')} style={styles.button}>
-            <Text style={styles.buttonText}> Click to Continue </Text>
-          </TouchableOpacity>
-          
-    </View>
-    //    </Router>
+            <Text style={styles.buttonText}> LogIn </Text>
+        </TouchableOpacity>
+      
+        </View>
   );
 }
 
@@ -35,6 +74,7 @@ const styles = StyleSheet.create({
   backgroundColor: '#FFFDD4'
   },
 logo: {
+
     width: 305,
     height: 159,
     marginBottom: 10,
@@ -55,3 +95,4 @@ buttonText: {
     color: '#fff',
 }
 });
+*/
